c399624d7c068e160ebce6913810b4618bc947d9
rifkidhan <rifkiramadhan.9428@hotmail.com>
2023-11-14 03:34:32 +0700

    test fix db url

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,17 +2,13 @@ import { Kysely, ParseJSONResultsPlugin } from "kysely";
 import { PlanetScaleDialect } from "kysely-planetscale";
 import { Database } from "#/db/schema.ts";
 
-const host = Deno.env.get("DATABASE_HOST");
-const username = Deno.env.get("DATABASE_USERNAME");
-const password = Deno.env.get("DATABASE_PASSWORD");
+const dbUrl = Deno.env.get("DATABASE_URL");
 
 const dialect = new PlanetScaleDialect({
-  host,
-  username,
-  password,
+	url: dbUrl,
 });
 
 export const db = new Kysely<Database>({
-  dialect,
-  plugins: [new ParseJSONResultsPlugin()],
+	dialect,
+	plugins: [new ParseJSONResultsPlugin()],
 });
